03d496ea19ece45bb51839a5b933c9f48e63abb2
arreeya <arreeya.s@facgure.com>
2024-06-21 23:01:24 +0700

    init project

---
+++ android/app/src/main/kotlin/com/chxmpxx/fibonacci_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chxmpxx.fibonacci_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/fibonacci_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FibonacciPage extends StatefulWidget {
+  const FibonacciPage({super.key});
+
+  @override
+  State<FibonacciPage> createState() => _FibonacciPageState();
+}
+
+class _FibonacciPageState extends State<FibonacciPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
